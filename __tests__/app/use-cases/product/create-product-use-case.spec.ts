aab3432c1ce28fe8b80fadb33c7ae674f9d4d05a
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2024-06-07 22:50:29 -0300

    test: fix wrong test

--- __tests__/app/use-cases/product/create-product-use-case.spec.ts
+++ __tests__/app/use-cases/product/create-product-use-case.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "bun:test";
-import { ProductDTO } from "../../../../src/app/dtos/product-dto";
+import type { CreateProductDTO } from "../../../../src/app/dtos/create-product-dto";
 import { CreateProductUseCase } from "../../../../src/app/use-cases/product/create-product-use-case";
 import { MockProductRepository } from "./mock-product-repository";
 
@@ -10,51 +10,22 @@ describe("CreateProductUseCase", () => {
       mockProductRepository,
     );
 
-    const productDTO = new ProductDTO(
-      "1",
-      "Notebook",
-      "NB001",
-      "A high-quality notebook",
-      "Stationery",
-    );
+    const productDTO: CreateProductDTO = {
+      name: "Notebook",
+      code: "NB001",
+      description: "A high-quality notebook",
+      category: "Stationery",
+    }
 
     await createProductUseCase.execute(productDTO);
 
-    const createdProduct = await mockProductRepository.findById("1");
+    const [createdProduct] = await mockProductRepository.findAll();
     expect(createdProduct).not.toBeNull();
-    expect(createdProduct?.id).toBe("1");
+    expect(createdProduct?.id).toBeDefined();
     expect(createdProduct?.name).toBe("Notebook");
     expect(createdProduct?.code).toBe("NB001");
     expect(createdProduct?.description).toBe("A high-quality notebook");
     expect(createdProduct?.category).toBe("Stationery");
   });
 
-  it("should not create a product with an existing id", async () => {
-    const mockProductRepository = new MockProductRepository();
-    const createProductUseCase = new CreateProductUseCase(
-      mockProductRepository,
-    );
-
-    const productDTO = new ProductDTO(
-      "1",
-      "Notebook",
-      "NB001",
-      "A high-quality notebook",
-      "Stationery",
-    );
-
-    await createProductUseCase.execute(productDTO);
-
-    const productDTO2 = new ProductDTO(
-      "1",
-      "Notebook",
-      "NB001",
-      "A high-quality notebook",
-      "Stationery",
-    );
-
-    expect(() => createProductUseCase.execute(productDTO2)).toThrow(
-      "Product with id 1 already exists",
-    );
-  });
 });
